b72e9d61f8cb53e464b24691c92e8c436b98cba7
john-vasile <john.Vs11@hotmail.com>
2022-11-27 13:19:36 -0500

    add constructors, setters, getters to student

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -5,9 +5,53 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class Student {
 
+
     private String name;
     private int studentId;
+    private static int nextStudentId=1;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public  Student(String name, int studentId){ this(name, studentId, 0,0);}
+    public  Student(String name){
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+    public double getGpa() {
+        return gpa;
+    }
+
+    public static void main(String[] args){
+        Student JohnV = new Student("John", 1, 1, 4.0);
+    }
 }
