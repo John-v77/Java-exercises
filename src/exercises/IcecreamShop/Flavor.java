9edf7d4ffde7426f67f0df97efcc8ce189e42c4d
john-vasile <john.Vs11@hotmail.com>
2022-11-28 09:00:41 -0500

    create Flavor & Cone class

---
+++ src/exercises/IcecreamShop/Flavor.java
@@ -0,0 +1,9 @@
+package exercises.IcecreamShop;
+
+import java.util.ArrayList;
+
+public class Flavor extends Ingredient {
+    public Flavor(String aName, double aCost, ArrayList<String> someAllergens){
+        super(aName, aCost, someAllergens);
+    }
+}
